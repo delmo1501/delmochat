ada2ce5204886d56854e35df2a9520b0523c93b4
Bruno <delmo123123@gmail.com>
2023-10-26 11:35:49 +0200

    starting chat with React + Tailwind

---
+++ src/components/Chat.jsx
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from 'react';
+import MessageList from './MessageList';
+import { io } from 'socket.io-client';
+
+const SERVER_URL = "http://localhost:3001"; // replace with your server URL
+
+function Chat() {
+    const [messages, setMessages] = useState([]);
+    const [inputValue, setInputValue] = useState('');
+    const [socket, setSocket] = useState(null);
+    console.log(messages);
+    useEffect(() => {
+        const fetchUsername = async () => {
+            const storedUsername = localStorage.getItem('username');
+            if (storedUsername) return storedUsername;
+
+            const response = await fetch('https://random-data-api.com/api/users/random_user');
+            const data = await response.json();
+            localStorage.setItem('username', data.username);
+            return data.username;
+        };
+
+        const username = fetchUsername();
+        const newSocket = io(SERVER_URL, { auth: { serverOffset: 0, username } });
+        setSocket(newSocket);
+
+        newSocket.on('chat message', (msg, serverOffset, username) => {
+            setMessages(prev => [...prev, { msg, serverOffset, username }]);
+        });
+
+        return () => {
+            newSocket.disconnect();
+        };
+    }, []);
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        if (inputValue && socket) {
+            socket.emit('chat message', inputValue);
+            setInputValue('');
+        }
+    };
+
+    return (
+        <section className="bg-white border rounded shadow-md max-w-md mx-auto mt-10 p-4">
+            <MessageList messages={messages} />
+            <form onSubmit={handleSubmit} className="flex">
+                <input 
+                    type="text" 
+                    value={inputValue}
+                    onChange={e => setInputValue(e.target.value)}
+                    placeholder="Type a message" 
+                    className="flex-grow p-2 mr-2 border rounded" 
+                />
+                <button type="submit" className="px-4 py-2 bg-blue-500 text-white rounded">Send</button>
+            </form>
+        </section>
+    );
+}
+
+export default Chat;
---
+++ src/components/MessageList.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function MessageList({ messages }) {
+    return (
+        <ul className="overflow-y-scroll h-96 mb-4">
+            {messages.map((message, index) => (
+                <li key={index} className={index % 2 === 0 ? "bg-black" : "bg-af8484"}>
+                    <p>{message.msg}</p>
+                    <small>{message.username}</small>
+                </li>
+            ))}
+        </ul>
+    );
+}
+
+export default MessageList;
