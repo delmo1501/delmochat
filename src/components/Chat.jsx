f6c47feff141732fb6f3ae35b5461fdf984697f1
Bruno <delmo123123@gmail.com>
2023-10-30 22:27:38 +0100

    trying heroku

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import MessageList from './MessageList';
 import { io } from 'socket.io-client';
 
-const SERVER_URL = process.env.REACT_APP_BACKEND_URL;
+const SERVER_URL = 'https://delmo-node-deploy-40a812c3b5f7.herokuapp.com/';
 
 function Chat() {
     const [messages, setMessages] = useState([]);
