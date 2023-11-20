ed24be81d63d23469b0917cb3020ec35c623a26d
Bruno <delmo123123@gmail.com>
2023-11-20 13:06:12 +0100

    trying authentification

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './App.css';
 import Chat from './components/Chat';
 import Header from './components/Header';
 import {NextUIProvider} from "@nextui-org/react";
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/api/user', { credentials: 'include' })
+      .then(response => response.json())
+      .then(data => setUser(data))
+      .catch(error => console.error('Error:', error));
+  }, []);
+  console.log(user);
   return (
     <NextUIProvider>
       <div className="w-full">
+        <h1 style={{ color: 'white', fontSize: '25px'}}>Hi {user?.username}!</h1>
         <Header />
-        <Chat />
+        <Chat user={user} />
       </div>
     </NextUIProvider>
   );
